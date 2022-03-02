356a9ca78fe4fdb0483846f1cf2fb04794623584
Endika Orube <endika.orube@biko2.com>
2022-03-02 15:53:28 +0100

    Feat: Mejora el filtro y añade alerta cuando no hay resultados

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,6 +13,15 @@ import { flowers } from "../resources/flowers";
 const Home: NextPage = ({ flowers }: any) => {
   const [inputText, setInputText] = useState<string>("");
 
+  const filteredFlowers: Flower[] = flowers.filter((flower: Flower) => {
+    const formatedInputText: string = inputText.toLowerCase();
+
+    return (
+      flower.name.toLowerCase().includes(formatedInputText) ||
+      flower.binomialName.toLowerCase().includes(formatedInputText)
+    );
+  });
+
   const onSearch = (event: any) => {
     setInputText(event.target.value);
   };
@@ -31,37 +40,37 @@ const Home: NextPage = ({ flowers }: any) => {
         <Search onSearch={onSearch} />
 
         <section className={styles.section}>
-          {flowers
-            .filter((flower: Flower) => {
-              return flower.name
-                .toLowerCase()
-                .includes(inputText.toLowerCase());
-            })
-            .map((flower: Flower) => {
-              return (
-                <Link key={flower.id} href={`/details/${flower.id}`}>
-                  <article className={styles.article}>
-                    <div className={styles.articleHeightInCm}>
-                      {flower.heightInCm} cm
-                    </div>
-                    <div className={styles.articleImgContainer}>
-                      <img
-                        src={flower.imgUrl}
-                        alt={flower.name}
-                        className={styles.articleImg}
-                      ></img>
-                    </div>
-                    <div className={styles.articleBody}>
-                      <p className={styles.articleBodyName}>{flower.name}</p>
-                      <p className={styles.articleBodyBinomialName}>
-                        {flower.binomialName}
-                      </p>
-                      <p className={styles.articleBodyPrice}>{flower.price}€</p>
-                    </div>
-                  </article>
-                </Link>
-              );
-            })}
+          {!filteredFlowers.length && (
+            <p className={styles.noSearchNotification}>
+              ¡Vaya! Parece que no hemos encontrado lo que buscabas.
+            </p>
+          )}
+
+          {filteredFlowers.map((flower: Flower) => {
+            return (
+              <Link key={flower.id} href={`/details/${flower.id}`}>
+                <article className={styles.article}>
+                  <div className={styles.articleHeightInCm}>
+                    {flower.heightInCm} cm
+                  </div>
+                  <div className={styles.articleImgContainer}>
+                    <img
+                      src={flower.imgUrl}
+                      alt={flower.name}
+                      className={styles.articleImg}
+                    ></img>
+                  </div>
+                  <div className={styles.articleBody}>
+                    <p className={styles.articleBodyName}>{flower.name}</p>
+                    <p className={styles.articleBodyBinomialName}>
+                      {flower.binomialName}
+                    </p>
+                    <p className={styles.articleBodyPrice}>{flower.price}€</p>
+                  </div>
+                </article>
+              </Link>
+            );
+          })}
         </section>
       </main>
     </>
