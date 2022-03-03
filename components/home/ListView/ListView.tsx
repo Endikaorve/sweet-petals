b9f47db8c63714bd6790184581132ecc6d5ef35c
Endika Orube <endika.orube@biko2.com>
2022-03-03 09:00:41 +0100

    Refactor: Extrae lógica principal de Home a nuevo componente ListView

---
+++ components/home/ListView/ListView.tsx
@@ -0,0 +1,66 @@
+import styles from "./ListView.module.css";
+
+import Link from "next/link";
+
+import Search from "../Search/Search";
+import { useState } from "react";
+import { Flower } from "../../../interfaces/interfaces";
+
+const ListView = ({ flowers }: { flowers: Flower[] }) => {
+  const [inputText, setInputText] = useState<string>("");
+
+  const filteredFlowers: Flower[] = flowers.filter((flower: Flower) => {
+    const formatedInputText: string = inputText.toLowerCase();
+
+    return (
+      flower.name.toLowerCase().includes(formatedInputText) ||
+      flower.binomialName.toLowerCase().includes(formatedInputText)
+    );
+  });
+
+  const onSearch = (event: any) => {
+    setInputText(event.target.value);
+  };
+
+  return (
+    <main className={styles.main}>
+      <Search onSearch={onSearch} />
+
+      <section className={styles.section}>
+        {!filteredFlowers.length && (
+          <p className={styles.noSearchNotification}>
+            ¡Vaya! Parece que no hemos encontrado lo que buscabas.
+          </p>
+        )}
+
+        {filteredFlowers.map((flower: Flower) => {
+          return (
+            <Link key={flower.id} href={`/details/${flower.id}`}>
+              <article className={styles.article}>
+                <div className={styles.articleHeightInCm}>
+                  {flower.heightInCm} cm
+                </div>
+                <div className={styles.articleImgContainer}>
+                  <img
+                    src={flower.imgUrl}
+                    alt={flower.name}
+                    className={styles.articleImg}
+                  ></img>
+                </div>
+                <div className={styles.articleBody}>
+                  <p className={styles.articleBodyName}>{flower.name}</p>
+                  <p className={styles.articleBodyBinomialName}>
+                    {flower.binomialName}
+                  </p>
+                  <p className={styles.articleBodyPrice}>{flower.price}€</p>
+                </div>
+              </article>
+            </Link>
+          );
+        })}
+      </section>
+    </main>
+  );
+};
+
+export default ListView;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,12 @@
-import styles from "../styles/Home.module.css";
-import { useState } from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
 
 import Header from "../components/shared/Header/Header";
-import Search from "../components/home/Search/Search";
+import ListView from "../components/home/ListView/ListView";
 
-import { Flower } from "../interfaces/interfaces";
 import { flowers } from "../resources/flowers";
 
 const Home: NextPage = ({ flowers }: any) => {
-  const [inputText, setInputText] = useState<string>("");
-
-  const filteredFlowers: Flower[] = flowers.filter((flower: Flower) => {
-    const formatedInputText: string = inputText.toLowerCase();
-
-    return (
-      flower.name.toLowerCase().includes(formatedInputText) ||
-      flower.binomialName.toLowerCase().includes(formatedInputText)
-    );
-  });
-
-  const onSearch = (event: any) => {
-    setInputText(event.target.value);
-  };
-
   return (
     <>
       <Head>
@@ -36,43 +17,7 @@ const Home: NextPage = ({ flowers }: any) => {
 
       <Header />
 
-      <main className={styles.main}>
-        <Search onSearch={onSearch} />
-
-        <section className={styles.section}>
-          {!filteredFlowers.length && (
-            <p className={styles.noSearchNotification}>
-              ¡Vaya! Parece que no hemos encontrado lo que buscabas.
-            </p>
-          )}
-
-          {filteredFlowers.map((flower: Flower) => {
-            return (
-              <Link key={flower.id} href={`/details/${flower.id}`}>
-                <article className={styles.article}>
-                  <div className={styles.articleHeightInCm}>
-                    {flower.heightInCm} cm
-                  </div>
-                  <div className={styles.articleImgContainer}>
-                    <img
-                      src={flower.imgUrl}
-                      alt={flower.name}
-                      className={styles.articleImg}
-                    ></img>
-                  </div>
-                  <div className={styles.articleBody}>
-                    <p className={styles.articleBodyName}>{flower.name}</p>
-                    <p className={styles.articleBodyBinomialName}>
-                      {flower.binomialName}
-                    </p>
-                    <p className={styles.articleBodyPrice}>{flower.price}€</p>
-                  </div>
-                </article>
-              </Link>
-            );
-          })}
-        </section>
-      </main>
+      <ListView flowers={flowers} />
     </>
   );
 };
