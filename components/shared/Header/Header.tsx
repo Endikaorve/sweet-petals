d3be7875a043e6dc0284792b76e6197be63fda64
Endika Orube <endika.orube@biko2.com>
2022-03-04 10:18:08 +0100

    Feat: Añade soporte para tema oscuro

--- components/shared/Header/Header.tsx
+++ components/shared/Header/Header.tsx
@@ -2,19 +2,43 @@ import styles from "./Header.module.css";
 
 import Link from "next/link";
 
-const Header = () => (
-  <header className={styles.header}>
-    <Link href="/">
-      <div className={styles.logo}>
-        <img
-          className={styles.logoImage}
-          src="/assets/img/logo.png"
-          alt="Sweet Petals"
-        />
-        <h1 className={styles.logoName}>Sweet Petals</h1>
-      </div>
-    </Link>
-  </header>
-);
+import { useTheme } from "next-themes";
+
+import LightThemeIcon from "components/icons/theme/Light/Light";
+import DarkThemeIcon from "components/icons/theme/Dark/Dark";
+
+const Header = () => {
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <header className={styles.header}>
+      <Link href="/">
+        <div className={styles.logo}>
+          <img
+            className={styles.logoImage}
+            src="/assets/img/logo.png"
+            alt="Sweet Petals"
+          />
+          <h1 className={styles.logoName}>Sweet Petals</h1>
+        </div>
+      </Link>
+
+      {theme === "dark" && (
+        <button
+          className={styles.themeButton}
+          onClick={() => setTheme("light")}
+        >
+          <LightThemeIcon></LightThemeIcon>
+        </button>
+      )}
+
+      {theme !== "dark" && (
+        <button className={styles.themeButton} onClick={() => setTheme("dark")}>
+          <DarkThemeIcon></DarkThemeIcon>
+        </button>
+      )}
+    </header>
+  );
+};
 
 export default Header;
