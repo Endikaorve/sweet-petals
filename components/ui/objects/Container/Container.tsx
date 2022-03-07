5f6cce9e54f8df95eeab2870bdc518db0f3f7686
Endika Orube <endika.orube@biko2.com>
2022-03-07 11:08:21 +0100

    Fix: Mejora el etiquetado del Container

--- components/ui/objects/Container/Container.tsx
+++ components/ui/objects/Container/Container.tsx
@@ -1,7 +1,7 @@
 import styles from "./Container.module.css";
 
 const Container: React.FC = ({ children }) => (
-  <div className={styles.mainContainer}>{children}</div>
+  <main className={styles.mainContainer}>{children}</main>
 );
 
 export default Container;
